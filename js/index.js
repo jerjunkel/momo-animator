e44b576488ce22412d4b1f3876d876cb46643c6d
jerjunkel <jerjunkel@gmail.com>
2020-11-02 00:58:36 -0500

    add fade-in section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 import momo from "./momo.js";
 
 const section = document.querySelector("section");
-momo(section, { speed: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
+momo(document, { speed: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
