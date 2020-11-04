e5cb6da1b36d96341f804e5968412798da56653f
jerjunkel <jerjunkel@gmail.com>
2020-11-04 01:14:25 -0500

    accepts element selector as root

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import momo from "./momo.js";
 
 const section = document.querySelector("section");
-momo(document, { duration: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
+// momo({ duration: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
+momo("h1", { duration: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
--- js/momo.js
+++ js/momo.js
@@ -1,17 +1,28 @@
-export default function (parent, options) {
-  if (typeof parent == "object" && options == undefined) {
-    options = parent;
-    parent = undefined;
+export default function (selector, options) {
+  if (typeof selector == "object" && options == undefined) {
+    options = selector;
+    selector = undefined;
   }
 
-  if (!parent) {
-    parent = document;
+  // Root element for momo animations
+  let root;
+
+  // Defaults to the body if no selector is passed in options
+  if (!selector) {
+    root = document.querySelector("body");
+  } else {
+    // Checks for selector type
+    if (typeof selector != "string")
+      throw Error("Selector needs to be a string");
+
+    root = document.querySelector(selector);
+    if (!root) throw Error(`No element found with the selector '${selector}'`);
   }
 
   let { duration, curve } = options;
-  console.log(options, parent);
+  console.log(options, root);
 
-  //   Check edge cases for duration option
+  // Check edge cases for duration option
   if (!duration) {
     duration = "2000";
   }
@@ -20,12 +31,12 @@ export default function (parent, options) {
     throw Error("Duration needs to be an interger value");
   }
 
-  //   Check edge case for curves
+  // Check edge case for curves
   if (!curve) {
     curve = "ease-in-out";
   }
 
-  const momoElements = parent.querySelectorAll(".momo");
+  const momoElements = root.querySelectorAll(".momo");
   addIntersectionObserver();
   addFadeIntersection();
 
@@ -44,15 +55,12 @@ export default function (parent, options) {
   }
 
   function addFadeIntersection() {
-    const momoFadeElements = parent.querySelectorAll(
-      `[data-animation^="fade"]`
-    );
+    const momoFadeElements = root.querySelectorAll(`[data-animation^="fade"]`);
 
     const observer = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach((entry) => {
           if (!entry.isIntersecting) return;
-
           entry.target.style.opacity = 0;
         });
       },
