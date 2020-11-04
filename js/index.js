100abcb96602e1f0694ee82d44c0edb15ce07f97
jerjunkel <jerjunkel@gmail.com>
2020-11-04 01:15:09 -0500

    remove test element

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ import momo from "./momo.js";
 
 const section = document.querySelector("section");
 // momo({ duration: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
-momo("h1", { duration: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
+momo({ duration: "2000", curve: "cubic-bezier(0.65, 0, 0.35, 1)" });
