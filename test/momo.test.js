5abe9094ecc06a042522e1850e1ada8a2e4a51ab
jerjunkel <jerjunkel@gmail.com>
2020-12-09 11:47:16 -0500

    add global option tests

--- test/momo.test.js
+++ test/momo.test.js
@@ -3,19 +3,31 @@ import Momo from "../src/js/Momo";
 
 describe("Momo global options", () => {
   test("setting default options", () => {
+    const defaultOptions = { duration: 1000, delay: 0, curve: "linear" };
     Momo.init();
-    expect(Momo.options.duration).toBe(1000);
-    expect(Momo.options.curve).toBe("linear");
-    expect(Momo.options.delay).toBe(0);
+    expect(Momo.options).toEqual(expect.objectContaining(defaultOptions));
   });
+
   test("setting user options", () => {
-    Momo.init({
+    const userSettings = {
       duration: 2000,
       delay: 200,
       curve: "cubic-bezier(0.83, 0, 0.17, 1)",
-    });
-    expect(Momo.options.duration).toBe(2000);
-    expect(Momo.options.curve).toBe("cubic-bezier(0.83, 0, 0.17, 1)");
-    expect(Momo.options.delay).toBe(200);
+    };
+    Momo.init(userSettings);
+    expect(Momo.options).toEqual(expect.objectContaining(userSettings));
+  });
+
+  test("removes staggerBy options if add by user", () => {
+    const userSettings = {
+      duration: 2000,
+      delay: 200,
+      curve: "cubic-bezier(0.83, 0, 0.17, 1)",
+      staggerBy: 200,
+    };
+    Momo.init(userSettings);
+    expect(Momo.options).toEqual(
+      expect.not.objectContaining({ staggerBy: 200 })
+    );
   });
 });
