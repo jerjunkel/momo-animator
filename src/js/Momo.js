9280a8a98f807f255a731ef9bdad6d848bfd4772
jerjunkel <jerjunkel@gmail.com>
2020-12-09 00:08:35 -0500

    add MomoAnimator file

--- src/js/Momo.js
+++ src/js/Momo.js
@@ -1,3 +1,4 @@
+import MomoAnimator from "./MomoAnimator.js";
 export default class Momo {
     constructor() {
         throw Error("Use the init method");
@@ -29,70 +30,6 @@ export default class Momo {
         const momoElements = Array.from(el.querySelectorAll(".momo"));
         const validOptions = options == null ? Momo.options : MomoOptionsChecker.check(options);
         return new MomoAnimator(momoElements, validOptions);
-    }
-}
-class MomoAnimator {
-    constructor(el, options) {
-        this.el = el;
-        this.options = options;
-        this.setup();
-    }
-    setup() {
-        this.addIntersectionObserver();
-        this.prepForFadeAnimation();
-    }
-    addIntersectionObserver() {
-        //   Observer for all momo elements in parent
-        const animate = (entry) => {
-            let timer;
-            let el = entry.target;
-            const animation = el.getAttribute("data-animation");
-            if (!animation)
-                return; // Exit if no animation is set
-            const delay = Number(el.getAttribute("data-animation-delay")) ||
-                this.options.delay;
-            const duration = Number(el.getAttribute("data-animation-duration")) ||
-                this.options.duration;
-            timer = setTimeout(() => {
-                el.removeAttribute("style");
-                clearTimeout(timer);
-            }, duration + delay + 100);
-            el.style.animation = `momo-${animation} ${this.options.curve} ${duration}ms ${delay}ms forwards`;
-        };
-        // Animation observer
-        this.createObserver(this.el, animate);
-    }
-    createObserver(elements, closure, options = {}) {
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach((entry) => {
-                if (!entry.isIntersecting)
-                    return;
-                closure(entry);
-                observer.unobserve(entry.target);
-            });
-        }, options);
-        // Check for more than one element
-        if (!Array.isArray(elements))
-            return observer.observe(elements);
-        elements.forEach((el) => {
-            observer.observe(el);
-        });
-    }
-    prepForFadeAnimation() {
-        var _a;
-        if (Array.isArray(this.el)) {
-            this.el.forEach((el) => {
-                var _a;
-                if ((_a = el.getAttribute("data-animation")) === null || _a === void 0 ? void 0 : _a.match(/^fade/g)) {
-                    el.style.opacity = "0";
-                }
-            });
-        }
-        else {
-            if ((_a = this.el.getAttribute("data-animation")) === null || _a === void 0 ? void 0 : _a.match(/^fade/g)) {
-                this.el.style.opacity = "0";
-            }
-        }
     }
 }
 class MomoOptionsChecker {
---
+++ src/ts/MomoAnimator.ts
@@ -0,0 +1,82 @@
+type MomoOptions = {
+  duration: number;
+  delay: number;
+  curve: string;
+};
+
+export default class MomoAnimator {
+  private el: HTMLElement | HTMLElement[];
+  private options: MomoOptions;
+
+  constructor(el: HTMLElement | HTMLElement[], options: MomoOptions) {
+    this.el = el;
+    this.options = options;
+    this.setup();
+  }
+
+  private setup() {
+    this.addIntersectionObserver();
+    this.prepForFadeAnimation();
+  }
+
+  private addIntersectionObserver() {
+    //   Observer for all momo elements in parent
+    const animate = (entry: IntersectionObserverEntry) => {
+      let timer: number;
+      let el = entry.target as HTMLElement;
+      const animation = el.getAttribute("data-animation");
+      if (!animation) return; // Exit if no animation is set
+      const delay =
+        Number(el.getAttribute("data-animation-delay") as string) ||
+        this.options.delay;
+      const duration =
+        Number(el.getAttribute("data-animation-duration") as string) ||
+        this.options.duration;
+
+      timer = setTimeout(() => {
+        el.removeAttribute("style");
+        clearTimeout(timer);
+      }, duration + delay + 100);
+
+      el.style.animation = `momo-${animation} ${this.options.curve} ${duration}ms ${delay}ms forwards`;
+    };
+
+    // Animation observer
+    this.createObserver(this.el, animate);
+  }
+
+  private createObserver(
+    elements: HTMLElement | HTMLElement[],
+    closure: (entry: IntersectionObserverEntry) => void,
+    options: object = {}
+  ) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        closure(entry);
+        observer.unobserve(entry.target);
+      });
+    }, options);
+
+    // Check for more than one element
+    if (!Array.isArray(elements)) return observer.observe(elements);
+
+    elements.forEach((el) => {
+      observer.observe(el);
+    });
+  }
+
+  private prepForFadeAnimation() {
+    if (Array.isArray(this.el)) {
+      this.el.forEach((el) => {
+        if (el.getAttribute("data-animation")?.match(/^fade/g)) {
+          el.style.opacity = "0";
+        }
+      });
+    } else {
+      if (this.el.getAttribute("data-animation")?.match(/^fade/g)) {
+        this.el.style.opacity = "0";
+      }
+    }
+  }
+}
