7bf82ef081e2ccb4d5cdc3fcb57e319202d0858e
jerjunkel <jerjunkel@gmail.com>
2020-12-10 13:18:35 -0500

    add new tests

--- test/momo.test.js
+++ test/momo.test.js
@@ -1,27 +1,23 @@
 import { TestScheduler } from "jest";
 import Momo from "../src/js/Momo";
 
-describe("Momo global options", () => {
-  test("setting default options", () => {
+describe("Testing Momo global options", () => {
+  test("default options are set", () => {
     const defaultOptions = { duration: 1000, delay: 0, curve: "linear" };
-    expect(Momo.getGlobalOptions()).toEqual(
-      expect.objectContaining(defaultOptions)
-    );
+    expect(Momo.getGlobalOptions()).toEqual(defaultOptions);
   });
 
-  test("setting user options", () => {
-    const userSettings = {
+  test("sets user options", () => {
+    const userOptions = {
       duration: 2000,
       delay: 200,
       curve: "cubic-bezier(0.83, 0, 0.17, 1)",
     };
-    Momo.setGlobalOptions(userSettings);
-    expect(Momo.getGlobalOptions()).toEqual(
-      expect.objectContaining(userSettings)
-    );
+    Momo.setGlobalOptions(userOptions);
+    expect(Momo.getGlobalOptions()).toEqual(userOptions);
   });
 
-  test("removes staggerBy options if add by user", () => {
+  test("removes staggerBy options if added by user", () => {
     const userSettings = {
       duration: 2000,
       delay: 200,
@@ -32,5 +28,11 @@ describe("Momo global options", () => {
     expect(Momo.getGlobalOptions()).toEqual(
       expect.not.objectContaining({ staggerBy: 200 })
     );
+  });
+
+  test("throws error if changed without setter method", () => {
+    expect(() => {
+      Momo._options = { duration: 3000, delay: 1, curve: "linear" };
+    }).toThrow();
   });
 });
