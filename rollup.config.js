fbf19c0569c9995655d0122eb762ed482700a9c0
jerjunkel <jerjunkel@gmail.com>
2021-02-20 19:49:16 -0500

    add typescript and rollup plugin as dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import typescript from "rollup-plugin-typescript";
 const dist = "dist";
 const bundle = "momo";
+
 export default {
-  input: "src/js/Momo.js",
+  input: "src/ts/momo.ts",
   output: {
     name: "Momo",
     file: `${dist}/${bundle}.umd.js`,
     format: "umd",
   },
+  plugins: [typescript()],
 };
